21e11d33cd1b4bf4a1e80971af4c30620c34d99b
Patrick Saade <git@cybert.rip>
2020-09-21 14:59:36 -0700

    Python print example
    Demonstrates how Python uses single and double quotes to print a string.

---
+++ python_print.py
@@ -0,0 +1,27 @@
+# these  examples will demonstrate how to print in Python
+#   it will also demonstrate how single and double quotes interact
+#   note: output will be in the comments before the code
+
+
+# in Python, any letters, numbers, symbols, etc. between a single or double quote is called a string
+# a string is simply defined as a sequence of characters
+#   for example: "hello" and 'hello' are considered strings
+
+
+# Hello World!
+#   print without quations
+#   two different ways and both are correct
+print("Hello World!")
+print('Hello World!')
+
+
+# 'Hello World'
+#   print with single quations
+#   make sure to use the double quotes first to sandwhich the single quotes
+print("'Hello World'")
+
+
+# "Hello World!"
+#   print with double quotes
+#   make sure to use the single quotes first to sandwhich the double quotes
+print('"Hello World!"')
